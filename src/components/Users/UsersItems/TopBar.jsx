e93552a8074540f4c3d973b6941ce3bb1dcda19b
Usama Hassan <64201456+itisusama@users.noreply.github.com>
2025-02-04 19:54:00 +0500

    Update TopBar.jsx

--- src/components/Users/UsersItems/TopBar.jsx
+++ src/components/Users/UsersItems/TopBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TopBar = () => {
+const TopBar = ({ searchQuery, setSearchQuery }) => {
   return (
     <section className='flex justify-between px-2'>
       <div><h2 className='font-bold text-2xl'>Users</h2></div>
@@ -12,7 +12,7 @@ const TopBar = () => {
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search" id="user-search" class="w-[18rem] block p-2 ps-10 text-sm border border-gray-400 rounded-full" placeholder="Search" required />
+        <input type="search" id="user-search" class="w-[18rem] block p-2 ps-10 text-sm border border-gray-400 rounded-full" placeholder="Search" required value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
         </form>
       </div>
     </section>
