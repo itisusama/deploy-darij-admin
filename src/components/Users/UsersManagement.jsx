109f48a819b4f37a0594578e96e65ffc22cb2716
Usama Hassan <64201456+itisusama@users.noreply.github.com>
2025-02-04 19:55:09 +0500

    Update UsersManagement.jsx

--- src/components/Users/UsersManagement.jsx
+++ src/components/Users/UsersManagement.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TopBar from './UsersItems/TopBar'
 import UsersTable from './UsersItems/UsersTable'
 
 const UsersManagement = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <div>
-      <TopBar/>
-      <UsersTable/>
+      <TopBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
+      <UsersTable searchQuery={searchQuery}/>
     </div>
   )
 }
