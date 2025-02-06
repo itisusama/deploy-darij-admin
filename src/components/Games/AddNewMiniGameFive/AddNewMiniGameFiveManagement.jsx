4f749945b073345896727657ab44456205471670
Usama Hassan <64201456+itisusama@users.noreply.github.com>
2025-02-06 12:25:42 +0500

    Update AddNewMiniGameFiveManagement.jsx

--- src/components/Games/AddNewMiniGameFive/AddNewMiniGameFiveManagement.jsx
+++ src/components/Games/AddNewMiniGameFive/AddNewMiniGameFiveManagement.jsx
@@ -63,7 +63,7 @@ const AddNewMiniGameFiveManagement = () => {
                                 <div
                                     className="w-[34px] h-[34px] mt-1 flex items-center justify-center bg-[#FFEAEA] border border-[#D80027] rounded-lg"
                                 >
-                                    <FaRegTrashAlt onClick={() => alert("Delete Dialogue")} className="text-[#D80027] cursor-pointer" style={{ fontSize: "16px" }} />
+                                    <FaRegTrashAlt onClick={() => alert("Functionality will be added soon")} className="text-[#D80027] cursor-pointer" style={{ fontSize: "16px" }} />
                                 </div>
                             </div>
                             <div className="mt-12 flex">
@@ -79,10 +79,9 @@ const AddNewMiniGameFiveManagement = () => {
                                         className="absolute top-0 right-0 bg-[#D80027] w-[12px] h-[12px] flex items-center justify-center rounded-full"
                                         title="Remove"
                                     >
-                                        <span className="text-white text-[10px] font-bold leading-none">×</span>
+                                        <span className="text-white text-[10px] font-bold leading-none" onClick={()=> setAudioFile(null)}>×</span>
                                     </div>
                                 </div>
-
 
                                 <input
                                     type="file"
@@ -92,75 +91,11 @@ const AddNewMiniGameFiveManagement = () => {
                                     onChange={handleAudioSelect}
                                 />
                             </div>
-                            <div className="mt-14">
-                                <label htmlFor="translate" className="font-inter font-[500] text-[16px] leading-6">
-                                    Translated Version
-                                </label>
-                                <div className="">
-                                    <input
-                                        type="text"
-                                        id="translate"
-                                        placeholder="Enter topic name"
-                                        className="w-[70%] h-[44px] outline-none border border-[#D0D5DD] rounded-lg px-3 py-2"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full h-[300px] border border-[#D0D5DD] rounded-lg mt-1">
-                        <div className="mx-5 mt-4">
-                            <label htmlFor="topic" className="font-inter font-[500] text-[16px] leading-6">
-                                French
-                            </label>
-                            <div className="flex">
-                                <input
-                                    type="text"
-                                    id="topic"
-                                    placeholder="Write dialogue in french"
-                                    className="w-[65%] h-[44px] outline-none border border-[#D0D5DD] rounded-lg px-3 py-2"
-                                />
-                                <select
-                                    name=""
-                                    id=""
-                                    className="w-[28%] h-[44px] mx-8 px-3 border border-[#D0D5DD] rounded-lg pr-4"
-                                >
-                                    <option value="Narrator">Narrator</option>
-                                    <option value="Man">Man</option>
-                                    <option value="Woman">Woman</option>
-                                </select>
-
-                                <div
-                                    className="w-[34px] h-[34px] mt-1 flex items-center justify-center bg-[#FFEAEA] border border-[#D80027] rounded-lg"
-                                >
-                                    <FaRegTrashAlt onClick={() => alert("Delete Dialogue")} className="text-[#D80027] cursor-pointer" style={{ fontSize: "16px" }} />
-                                </div>
-                            </div>
-                            <div className="mt-12 flex">
-                                <div className="flex mt-3" onClick={() => document.getElementById('audio-input').click()}>
-                                    <IoIosAddCircleOutline className='w-[20px] h-[20px] cursor-pointer' />
-                                    <p className='text-[#000000] underline font-inter font-[400] cursor-pointer text-[16px] leading-6 '>Add Audio</p>
-                                </div>
-                                <div
-                                    className="relative w-[34px] h-[34px] mt-1 ml-14 flex items-center justify-center bg-[#E2E2E2] rounded-full cursor-pointer"
-                                >
-                                    <IoVolumeHighOutline className="w-[20px] h-[20px] text-[#333333]" />
-                                    <div
-                                        className="absolute top-0 right-0 bg-[#D80027] w-[12px] h-[12px] flex items-center justify-center rounded-full"
-                                        title="Remove"
-                                    >
-                                        <span className="text-white text-[10px] font-bold leading-none">×</span>
-                                    </div>
-                                </div>
-
-
-                                <input
-                                    type="file"
-                                    id="audio-input"
-                                    accept="audio/*"
-                                    className="hidden"
-                                    onChange={handleAudioSelect}
-                                />
-                            </div>
+                            {audioFile && (
+                                    <p className="mt-2 text-green-600 font-inter text-[14px]">
+                                        {audioFile.name}
+                                    </p>
+                                    )}
                             <div className="mt-14">
                                 <label htmlFor="translate" className="font-inter font-[500] text-[16px] leading-6">
                                     Translated Version
@@ -180,7 +115,7 @@ const AddNewMiniGameFiveManagement = () => {
 
                 <div className="ml-5">
                     <button
-                        onClick={() => alert("Do you want to add more?")}
+                        onClick={() => alert("Functionality will be added soon")}
                         className="w-[210px] h-[44px] mt-5 rounded-[100px] px-4 py-2 bg-[#FFF1B2] font-[600] text-[16px] leading-[24px] flex items-center justify-center space-x-2"
                     >
                         <IoMdAdd className="text-black" />
@@ -194,7 +129,7 @@ const AddNewMiniGameFiveManagement = () => {
                         Cancel
                     </button>
                     </Link>
-                    <button className="w-[210px] h-[44px] mr-4 rounded-[100px] px-4 py-2 bg-[#FFC600] font-[600] text-[16px] leading-[24px] flex items-center justify-center space-x-2">
+                    <button className="w-[210px] h-[44px] mr-4 rounded-[100px] px-4 py-2 bg-[#FFC600] font-[600] text-[16px] leading-[24px] flex items-center justify-center space-x-2" onClick={()=> alert("Functionality will be added soon")}>
                         Add
                     </button>
                 </div>
